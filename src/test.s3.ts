d965edc97d3463578ae8fc91d05f4eb5517bcba0
tassun <32536248+tassun@users.noreply.github.com>
2024-10-30 15:23:56 +0700

    initial project

---
+++ src/test.s3.ts
@@ -0,0 +1,31 @@
+import { S3Client, ListBucketsCommand } from '@aws-sdk/client-s3';
+
+const client = new S3Client({
+    region: 'ap-southeast-1',
+});
+
+async function listBucket() {
+    const params = {
+    };
+    const command = new ListBucketsCommand(params);    
+    try {        
+        const data = await client.send(command);
+    } catch(ex) {
+        console.error(ex);
+    }
+}
+
+listBucket();
+
+/*
+using environment variables:
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_SESSION_TOKEN
+AWS_REGION
+ex.
+set AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
+set AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+set AWS_SESSION_TOKEN=AQoEXAMPLEH4aoAH0gNCAPy...truncated...zrkuWJOgQs8IZZaIv2BXIa2R4Olgk
+set AWS_REGION=us-west-2
+*/
